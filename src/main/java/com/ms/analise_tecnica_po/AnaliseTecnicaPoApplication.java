617960b69faab0c57a9e8717d787d97fc76a61b4
Fabio Brasil <ffonseca@personalcard.com.br>
2023-12-14 16:49:21 -0300

    creating database configuration

---
+++ src/main/java/com/ms/analise_tecnica_po/AnaliseTecnicaPoApplication.java
@@ -0,0 +1,13 @@
+package com.ms.analise_tecnica_po;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseTecnicaPoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliseTecnicaPoApplication.class, args);
+	}
+
+}

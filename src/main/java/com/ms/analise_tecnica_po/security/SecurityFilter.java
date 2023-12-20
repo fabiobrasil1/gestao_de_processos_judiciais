edfd2e757d1ffe0d95221236967f3a1e4caa026c
Fabio Brasil <ffonseca@personalcard.com.br>
2023-12-19 21:06:30 -0300

    fix security filter

--- src/main/java/com/ms/analise_tecnica_po/security/SecurityFilter.java
+++ src/main/java/com/ms/analise_tecnica_po/security/SecurityFilter.java
@@ -15,6 +15,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class SecurityFilter extends OncePerRequestFilter {
   @Autowired
   private UserRepository userRepository;
